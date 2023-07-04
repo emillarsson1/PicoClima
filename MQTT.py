2ac20b6c31d2bc4771e8592ae737dc58295ca608
emillarsson1 <96737083+emillarsson1@users.noreply.github.com>
2023-07-04 22:25:09 +0200

    Update MQTT.py

--- MQTT.py
+++ MQTT.py
@@ -1,18 +1,18 @@
 import time
 import ubinascii
 import machine
-from machine import Pin
-import dht
-#import ujson
 from umqtt.simple import MQTTClient
+import sensors
 
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 MQTT_BROKER ="io.adafruit.com"
 PORT = 1883
-ADAFRUIT_USERNAME = "Emil1985"
-ADAFRUIT_PASSWORD ="aio_SEmj26QYQaCKhgHSGwvH8oB9Ek9d"
-TEMPERATURE_TOPIC = b"Emil1985/feeds/temperature-and-humidity.temperature"
-HUMIDITY_TOPIC = b"Emil1985/feeds/temperature-and-humidity.humidity"
+ADAFRUIT_USERNAME = "USERNAME HERE"
+ADAFRUIT_PASSWORD ="PASSWORD HERE"
+TEMPERATURE_TOPIC = b"TEMPERATURE TOPIC HERE"
+HUMIDITY_TOPIC = b"HUMIDITY TOPIC HERE"
+SOIL_TEMPERATURE_TOPIC=b"SOIL TEMPERATURE TOPIC HERE"
+SOIL_HUMIDITY_TOPIC=b"SOIL HUMIDITY TOPIC HERE"
 publish_interval=60
 last_publish=time.time()
 
@@ -21,13 +21,6 @@ def reset():
     print("Resetting...")
     time.sleep(5)
     machine.reset()
-
-def get_temperature_and_humidity():
-    sensor = dht.DHT11(Pin(27))
-    sensor.measure()
-    temp = sensor.temperature()
-    hum = sensor.humidity()
-    return(temp,hum)
 
 def start_mqtt():
     print(f"Starting connection with MQTT Broker :: {MQTT_BROKER}")
@@ -40,15 +33,20 @@ def start_mqtt():
         global last_publish
 
         if(time.time() - last_publish) >= publish_interval:
-            temp_and_humidity = get_temperature_and_humidity()
+            temp_and_humidity = sensors.get_temperature_and_humidity()
+            temp_and_moisture = sensors.get_soil_temperature_and_humidity()
+            
             temp = temp_and_humidity[0]
             humidity = temp_and_humidity[1]
+            
+            soil_temp=temp_and_moisture[0]
+            soil_moisture=temp_and_moisture[1]
 
-           # Not used - created two feeds instead
-           # temp_and_humidity = ujson.dumps(temp_and_humidity)
-           
             mqttClient.publish(TEMPERATURE_TOPIC, str(temp))
             mqttClient.publish(HUMIDITY_TOPIC, str(humidity))
+            mqttClient.publish(SOIL_TEMPERATURE_TOPIC, str(soil_temp))
+            mqttClient.publish(SOIL_HUMIDITY_TOPIC, str(soil_moisture))
+            
             last_publish=time.time()
             local_time = time.localtime()
             date_time_str = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(
@@ -60,4 +58,6 @@ def start_mqtt():
             local_time[5]   # second
             )
             print("The data was sent at " + str(date_time_str))
+            
+
         time.sleep(10)
