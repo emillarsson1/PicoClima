8513026ac75f603fb5d7dac97c80b41d8ce1eb68
emillarsson1 <96737083+emillarsson1@users.noreply.github.com>
2023-07-09 19:56:37 +0200

    Update sensors.py

--- code/sensors.py
+++ code/sensors.py
@@ -4,6 +4,11 @@ import stemma_soil_sensor
 
 from machine import Pin, I2C
 
+''' 
+Function that retrieves indoor humidity and temperature
+from the DHT11 sensor
+'''
+
 def get_temperature_and_humidity():
     sensor = dht.DHT11(Pin(27))
     sensor.measure()
@@ -11,10 +16,15 @@ def get_temperature_and_humidity():
     hum = sensor.humidity()
     return(temp,hum)
 
+'''
+Function that retrieves soil moisture and temperature
+from the Adafruit STEMMA soil sensor
+'''
+
 def get_soil_temperature_and_humidity():
-        i2c = I2C(0, scl=Pin(17), sda=Pin(16))
-        SENSOR_ADDR = 0x36
-        seesaw2 = stemma_soil_sensor.StemmaSoilSensor(i2c)
-        moisture = seesaw2.get_moisture()
-        temperature = seesaw2.get_temp()
-        return(temperature,moisture)
+    i2c = I2C(0, scl=Pin(17), sda=Pin(16))
+    SENSOR_ADDR = 0x36
+    seesaw2 = stemma_soil_sensor.StemmaSoilSensor(i2c)
+    moisture = seesaw2.get_moisture()
+    temperature = seesaw2.get_temp()
+    return(temperature,moisture)
